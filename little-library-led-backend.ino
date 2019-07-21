29f9a7321c99b46f15e09c8eeadc83e820cbad8e
gidoBOSSftw5731 <gido5731@gido-desk-deb.imagen.click>
2019-07-21 02:58:01 -0400

    I. HATE. C++

--- little-library-led-backend.ino
+++ little-library-led-backend.ino
@@ -1,10 +1,26 @@
+#include <ArduinoJson.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 
 const char* ssid = "AS701.NET";
 const char* password = "wow-wow-wubbzy";
 
-HTTPClient http;
+HTTPClient client;
+
+// data is a struct for holding the imput JSON data
+struct data {
+  char* red;
+  char* green;
+  char* blue;
+
+};
+
+#define server "http://pastebin.com/raw/5Qg6AzhP"
+
+const unsigned long HTTP_TIMEOUT = 10000;  // max respone time from server
+const size_t MAX_CONTENT_SIZE = 512;       // max size of the HTTP response
+
+
 
 void setup() {
   WiFi.begin(ssid, password);
@@ -18,16 +34,63 @@ void setup() {
 }
 
 void loop() {
-  Serial.println(WiFi.localIP());
+   if(connect(server)) {
+    if(sendRequest(server)) {
+      data data;
+      readResponseContent(&data);
+      printData(&data);
+      
+    }
+  }
 
-  http.begin("http://restoncoffeemachine.appspot.com/");
-  http.addHeader("Content-Type", "application/octet-stream");
-  
-  int httpCode = http.POST("Message from ESP8266");
-  String payload = http.getString();
 
-  Serial.println(payload);
+  client.end();
+
+}
+
+void printData(const struct data* data) {
+  Serial.print("red: ");
+  Serial.println(data->red);
+  Serial.print("green: ");
+  Serial.println(data->green);
+  Serial.print("blue: ");
+  Serial.println(data->blue);
+}
+
+void readResponseContent(struct data* input) {
+
+    DynamicJsonDocument root(JSON_OBJECT_SIZE(3));
+    deserializeJson(root, input);
+    //JsonObject& root = buf.parseObject(data);
+
+    strcpy(input->red, root["red"]);
+    strcpy(input->green, root["green"]);
+    strcpy(input->blue, root["blue"]);
+
+} 
 
 
-  http.end();
+bool sendRequest(const char* host) {
+  Serial.print("GET ");
+  Serial.println(host);
+  /*
+  client.print("GET ");
+  client.print(resource);
+  client.println(" HTTP/1.1");
+  client.print("Host: ");
+  client.println(host);
+  client.println("Connection: close");
+  client.println();*/
+
+  return true;
+}
+
+bool connect(const char* hostName) {
+  Serial.print("Connect to ");
+  Serial.println(hostName);
+
+  bool ok = client.begin(hostName);
+
+  Serial.println(ok ? "Connected" : "Connection Failed!");
+  return ok;
 }
