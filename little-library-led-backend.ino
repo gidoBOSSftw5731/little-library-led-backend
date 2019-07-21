c6af47cdf41bf6a9cec1fd67d7843d3ea289312d
gidoBOSSftw5731 <gido5731@gido-desk-deb.imagen.click>
2019-07-21 01:00:11 -0400

    testing HTTP  downloads. no HTTPS and I dont care enough to add it

--- little-library-led-backend.ino
+++ little-library-led-backend.ino
@@ -1,11 +1,14 @@
 #include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
 
 const char* ssid = "AS701.NET";
 const char* password = "wow-wow-wubbzy";
 
+HTTPClient http;
+
 void setup() {
   WiFi.begin(ssid, password);
-  Serial.begin(9600);
+  Serial.begin(115200);
   while (WiFi.status() != WL_CONNECTED) {
   
     delay(1000);
@@ -16,4 +19,15 @@ void setup() {
 
 void loop() {
   Serial.println(WiFi.localIP());
+
+  http.begin("http://restoncoffeemachine.appspot.com/");
+  http.addHeader("Content-Type", "application/octet-stream");
+  
+  int httpCode = http.POST("Message from ESP8266");
+  String payload = http.getString();
+
+  Serial.println(payload);
+
+
+  http.end();
 }
