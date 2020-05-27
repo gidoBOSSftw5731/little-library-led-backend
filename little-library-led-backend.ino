95acea4cd7f82781020fb008774320b96e222101
gido5731 <celtictarot386@gmail.com>
2020-05-27 01:27:51 -0400

    downloads to console as int

--- little-library-led-backend.ino
+++ little-library-led-backend.ino
@@ -1,21 +1,22 @@
-#include <ArduinoJson.h>
+// solar panels max space 29x17 inch
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
+//#include <Dns.h>
 
 const char* ssid = "AS701.NET";
 const char* password = "wow-wow-wubbzy";
 
-HTTPClient client;
-
 // data is a struct for holding the imput JSON data
 struct data {
-  char* red;
-  char* green;
-  char* blue;
+  int red;
+  int green;
+  int blue;
 
 };
 
-#define server "http://pastebin.com/raw/5Qg6AzhP"
+#define redURL "http://llib.clickable.systems/r"
+#define greenURL "http://llib.clickable.systems/g"
+#define blueURL "http://llib.clickable.systems/b"
 
 const unsigned long HTTP_TIMEOUT = 10000;  // max respone time from server
 const size_t MAX_CONTENT_SIZE = 512;       // max size of the HTTP response
@@ -31,66 +32,78 @@ void setup() {
     Serial.println("Connecting..");
   
   }
+  
+}
+
+data getColors() {
+
+  data colors;
+
+  chkRed(&colors);
+  chkGreen(&colors);
+  chkBlue(&colors);
+
+  
+
+  return colors;
+}
+
+void chkRed(data* obj) {
+  HTTPClient client;
+
+  client.begin(redURL);
+
+  int httpCode = client.GET();//Send the request
+ 
+  String color = "N/A";
+
+  
+
+  color = client.getString();
+ 
+
+  
+  client.end();
+  
+  //Serial.println(color);
+
+  obj->red = color.toInt();
+}
+
+void chkGreen(data* obj) {
+  HTTPClient client;
+
+  client.begin(greenURL);
+  
+  int httpCode = client.GET();//Send the request
+ 
+  String color = "N/A";
+
+  color = client.getString();
+  
+  client.end();
+
+  obj->green = color.toInt();
+}
+
+void chkBlue(data* obj) {
+  HTTPClient client;
+
+  client.begin(blueURL);
+
+  int httpCode = client.GET();//Send the request
+ 
+  String color = "N/A";
+
+  color = client.getString();
+  
+  client.end();
+
+  obj->blue = color.toInt();
 }
 
 void loop() {
-   if(connect(server)) {
-    if(sendRequest(server)) {
-      data data;
-      readResponseContent(&data);
-      printData(&data);
-      
-    }
-  }
-
-
-  client.end();
-
-}
-
-void printData(const struct data* data) {
-  Serial.print("red: ");
-  Serial.println(data->red);
-  Serial.print("green: ");
-  Serial.println(data->green);
-  Serial.print("blue: ");
-  Serial.println(data->blue);
-}
-
-void readResponseContent(struct data* input) {
-
-    DynamicJsonDocument root(JSON_OBJECT_SIZE(3));
-    deserializeJson(root, input);
-    //JsonObject& root = buf.parseObject(data);
-
-    strcpy(input->red, root["red"]);
-    strcpy(input->green, root["green"]);
-    strcpy(input->blue, root["blue"]);
-
-} 
-
-
-bool sendRequest(const char* host) {
-  Serial.print("GET ");
-  Serial.println(host);
-  /*
-  client.print("GET ");
-  client.print(resource);
-  client.println(" HTTP/1.1");
-  client.print("Host: ");
-  client.println(host);
-  client.println("Connection: close");
-  client.println();*/
-
-  return true;
-}
-
-bool connect(const char* hostName) {
-  Serial.print("Connect to ");
-  Serial.println(hostName);
-
-  bool ok = client.begin(hostName);
-
-  Serial.println(ok ? "Connected" : "Connection Failed!");
-  return ok;
+  data colors = getColors();
+  Serial.println(colors.blue);
+  delay(1000);
 }
